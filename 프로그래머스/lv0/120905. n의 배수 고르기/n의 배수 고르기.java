19d973122d58be563aa6fb6349baf35e90846195
Subeen Jang <121777490+Subeen-Jang@users.noreply.github.com>
2023-02-26 00:07:07 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+       int[] answer = {};
+        int count=0;
+        int j=0;
+        
+        for(int i=0;i<numlist.length;i++){
+            if(numlist[i]%n==0){
+              count++;
+            }
+        }
+        
+      answer = new int[count];
+        for(int i=0;i<numlist.length;i++){
+            if(numlist[i]%n==0){
+              answer[j] = numlist[i];
+                j++;
+        }
+        
+    }
+        return answer;
+}
+}
