adecb1dff6a86ac0f1dd60aba8dbe6ac6f1bd252
Subeen Jang <121777490+Subeen-Jang@users.noreply.github.com>
2023-03-05 01:24:21 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 8.53 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char temp;
+        
+        char c[] = my_string.toCharArray();
+        temp = c[num1];
+        c[num1] = c[num2];
+        c[num2] = temp;
+        
+        for(int i=0;i<c.length;i++){
+            answer += c[i];
+        }
+        return answer;
+    }
+}
