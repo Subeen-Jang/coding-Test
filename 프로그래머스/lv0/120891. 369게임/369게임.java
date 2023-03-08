4b0b3e3381370718f68f25377f032f9c811e150c
Subeen Jang <121777490+Subeen-Jang@users.noreply.github.com>
2023-03-08 17:45:11 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        int num;
+        while(order!=0){
+            num=order%10;
+            order/=10;
+            if(num!=0&&num%3==0){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
