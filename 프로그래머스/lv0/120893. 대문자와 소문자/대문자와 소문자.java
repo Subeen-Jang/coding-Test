ff596cdf301ff46dbf48cea9fa7c7c27abdc368c
Subeen Jang <121777490+Subeen-Jang@users.noreply.github.com>
2023-03-17 22:56:08 +0900

    [level 0] Title: 대문자와 소문자, Time: 11.96 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -4,7 +4,6 @@ class Solution {
         for(int i=0;i<my_string.length();i++){
             if(Character.isUpperCase(my_string.charAt(i))){
                   answer+=Character.toLowerCase(my_string.charAt(i));
-               
             }else{
                answer += Character.toUpperCase(my_string.charAt(i));
             }
