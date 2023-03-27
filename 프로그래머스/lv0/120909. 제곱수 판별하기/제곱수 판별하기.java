b5bea00f267ce6ebd497774dcdbd395c3dcf2e2a
Subeen Jang <121777490+Subeen-Jang@users.noreply.github.com>
2023-03-27 23:41:51 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int Sqrt=(int)Math.sqrt(n);
+        if(Sqrt*Sqrt==n){
+            return 1;
+        }else{return 2;}
+    }
+}
