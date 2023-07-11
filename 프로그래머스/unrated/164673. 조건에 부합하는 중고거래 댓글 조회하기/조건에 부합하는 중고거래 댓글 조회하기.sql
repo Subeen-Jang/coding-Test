eb2879c585c3bc55dd5352b7e03543c43b78e7aa
장수빈 <121777490+Subeen-Jang@users.noreply.github.com>
2023-07-11 23:14:52 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -8,10 +8,3 @@ FROM used_goods_board a RIGHT OUTER JOIN used_goods_reply b
 ON a.BOARD_ID = b.BOARD_ID
 where a.created_date > to_date('20220930','yyyy-mm-dd') and a.created_date < to_date('20221101','yyyy-mm-dd')
 order by b.created_date, a.title
-
-
--- SELECT B.TITLE AS TITLE, B.BOARD_ID, R.REPLY_ID, R.WRITER_ID, R.CONTENTS, TO_CHAR(R.CREATED_DATE, 'YYYY-MM-DD') AS CREATED_DATE
--- FROM USED_GOODS_REPLY R LEFT OUTER JOIN USED_GOODS_BOARD B
--- ON R.BOARD_ID = B.BOARD_ID
--- WHERE B.CREATED_DATE > TO_DATE('20220930', 'YYYY-MM-DD') AND B.CREATED_DATE < TO_DATE('20221101','YYYY-MM-DD') 
--- ORDER BY R.CREATED_DATE ASC, B.TITLE ASC;
