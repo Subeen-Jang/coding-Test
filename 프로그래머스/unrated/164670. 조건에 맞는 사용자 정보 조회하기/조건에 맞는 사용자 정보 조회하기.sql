e2c80fc227370fbdce049324eb07ef9eea165b88
장수빈 <121777490+Subeen-Jang@users.noreply.github.com>
2023-08-01 01:33:25 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT 
+    user_id, 
+    nickname, 
+    city||' '|| street_address1||' '|| street_address2 as 전체주소, 
+    substr(tlno,0,3)||'-'||substr(tlno,4,4)||'-'|| substr(tlno,8)as 전화번호
+from used_goods_user
+where user_id in 
+    (select writer_id from used_goods_board group by writer_id having count(board_id) >=3 )
+order by user_id desc
