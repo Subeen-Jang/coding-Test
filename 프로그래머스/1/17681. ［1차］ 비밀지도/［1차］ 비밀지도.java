d4e0205cf25aebd91fac88c7d39aad0873962b75
장수빈 <121777490+Subeen-Jang@users.noreply.github.com>
2023-10-14 19:03:08 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.80 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,18 +1,17 @@
+import java.util.*;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[arr1.length];
-
+        String[] result = new String[arr1.length];
+        
         for(int i=0;i<arr1.length;i++){
-            String str = Integer.toBinaryString(arr1[i]|arr2[i]);
             
-             while (str.length() < n) {
-                str = "0" + str;
-            }
+            result[i] = String.format("%0"+n+"d",Long.parseLong(Integer.toBinaryString(arr1[i] | arr2[i])));
 
             answer[i] = "";
             
-            for(int j=0;j<n;j++){
-                if(str.charAt(j)=='1'){
+            for(int j=0;j<result[i].length();j++){
+                if(result[i].charAt(j)=='1'){
                 answer[i] += "#";
                 }else{
                 answer[i] +=" ";
