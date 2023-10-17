8153128fbbd3754b75a32c2b086dfc61f981dc0d
장수빈 <121777490+Subeen-Jang@users.noreply.github.com>
2023-10-18 00:04:35 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while(n>=a){
+            int bottle = (n/a)*b;
+
+            answer += bottle;
+            
+            n = (n%a) + bottle;    
+        }
+        
+        return answer;
+    }
+}
