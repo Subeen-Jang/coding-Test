0f50121f4f0d443b1137b506cc9bfc44af7800e8
장수빈 <121777490+Subeen-Jang@users.noreply.github.com>
2023-10-29 00:01:44 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.25 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        List<Integer> list = new ArrayList<>(score.length);
+        int min = score[0];
+        
+        for(int i=0;i<score.length;i++){
+            list.add(score[i]);
+            Collections.sort(list, Collections.reverseOrder());
+            
+            if(i<k){
+                answer[i] = list.get(i);
+            }else{
+                answer[i] = list.get(k-1);
+            }
+        }
+        
+
+        return answer;
+    }
+}
