c875c1b4f05e265238445f69e34e89e2c000bb6d
장수빈 <121777490+Subeen-Jang@users.noreply.github.com>
2023-11-07 17:36:06 +0900

    [level 1] Title: 덧칠하기, Time: 1.06 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        
+        int start = section[0]; 
+        answer++;
+          
+        for(int i:section){
+            if(start + m > i){
+                continue;
+            }else{
+                start = i;
+                answer++;
+            }
+
+        }
+        return answer;
+    }
+}
+
+
