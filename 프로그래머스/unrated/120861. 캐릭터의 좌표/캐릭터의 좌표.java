f27e624091bc3b9fa9e0067c184c85c14b125e5a
장수빈 <121777490+Subeen-Jang@users.noreply.github.com>
2023-12-08 22:40:19 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {0, 0};
+        int maxX = board[0] / 2; 
+        int maxY = board[1] / 2;
+        
+        for (String tmp : keyinput) {
+            switch (tmp) {
+                case "up":
+                    answer[1] = Math.min(answer[1] + 1, maxY);
+                    break;
+                case "down":
+                    answer[1] = Math.max(answer[1] - 1, -maxY);
+                    break;
+                case "right":
+                    answer[0] = Math.min(answer[0] + 1, maxX);
+                    break;
+                case "left":
+                    answer[0] = Math.max(answer[0] - 1, -maxX);
+            }
+        }
+
+        return answer;
+    }
+}
