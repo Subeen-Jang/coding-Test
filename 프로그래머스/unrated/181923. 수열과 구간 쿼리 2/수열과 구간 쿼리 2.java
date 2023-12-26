57631a008c43812e8dbc3f33bb2e45359723b7d2
장수빈 <121777490+Subeen-Jang@users.noreply.github.com>
2023-12-26 22:25:46 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 3.02 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        
+        
+        for(int i=0;i<queries.length;i++){
+            int min = 1000000;
+            for(int j = queries[i][0];j<=queries[i][1];j++){
+                
+                if(arr[j] > queries[i][2]){
+                    min = Math.min(min, arr[j]);
+                }
+            }
+            
+            answer[i] = min == 1000000? -1:min;
+            
+        }
+        return answer;
+    }
+}
